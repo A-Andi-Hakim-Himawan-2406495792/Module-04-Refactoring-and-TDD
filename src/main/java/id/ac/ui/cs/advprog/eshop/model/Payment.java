2833e5d7cbb8ae6e01704e8c62a99bc081e9e3dc
unknown <hakimandi976@gmail.com>
2026-03-10 10:48:51 +0700

    [REFACTOR] Use PaymentMethod and PaymentStatus enums in Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import lombok.Getter;
 import java.util.Map;
 
@@ -17,28 +19,38 @@ public class Payment {
         this.paymentData = paymentData;
         this.order = order;
 
-        if ("VOUCHER_CODE".equals(method)) {
+        // REFACTOR: Menggunakan Enum pengganti string hardcode
+        if (PaymentMethod.VOUCHER_CODE.getValue().equals(method)) {
             validateVoucherCode(paymentData.get("voucherCode"));
-        } else if ("BANK_TRANSFER".equals(method)) {
+        } else if (PaymentMethod.BANK_TRANSFER.getValue().equals(method)) {
             validateBankTransfer(paymentData.get("bankName"), paymentData.get("referenceCode"));
         } else {
-            this.status = "REJECTED";
+            this.status = PaymentStatus.REJECTED.getValue();
+        }
+    }
+
+    public void setStatus(String status) {
+        // REFACTOR: Menambahkan validasi status menggunakan Enum
+        if (PaymentStatus.contains(status)) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
         }
     }
 
     private void validateVoucherCode(String voucher) {
         if (voucher != null && voucher.length() == 16 && voucher.startsWith("ESHOP") && countDigits(voucher) == 8) {
-            this.status = "SUCCESS";
+            this.status = PaymentStatus.SUCCESS.getValue();
         } else {
-            this.status = "REJECTED";
+            this.status = PaymentStatus.REJECTED.getValue();
         }
     }
 
     private void validateBankTransfer(String bankName, String referenceCode) {
         if (bankName != null && !bankName.trim().isEmpty() && referenceCode != null && !referenceCode.trim().isEmpty()) {
-            this.status = "SUCCESS";
+            this.status = PaymentStatus.SUCCESS.getValue();
         } else {
-            this.status = "REJECTED";
+            this.status = PaymentStatus.REJECTED.getValue();
         }
     }
 
